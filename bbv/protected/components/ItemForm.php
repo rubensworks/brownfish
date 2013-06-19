fa66f8852f17816e00e2a66e8985c5830dea60ab
Ruben Taelman <rubensworks@gmail.com>
2013-06-19 17:35:56 +0200

    Added ItemForm param comment + Added the Composite Form Extension for better form displays with related models + Updated .gitignore to not add item data files.

--- bbv/protected/components/ItemForm.php
+++ bbv/protected/components/ItemForm.php
@@ -7,6 +7,8 @@
  * 
  * @param $model The extended model from item, so $model->item should be present with a link to a real
  * Item class instance.
+ * @param $view A view that will be rendered after the common form elements for an Item. $form and $model
+ * will be passed trough to this view.
  *
  */
 class ItemForm extends CWidget
