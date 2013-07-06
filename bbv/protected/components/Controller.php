fb11698c91f578fab0dd5bb28d6467e5fdf0c8ad
Ruben Taelman <rubensworks@gmail.com>
2013-07-06 17:02:20 +0200

    Added simple Google Analytics tracking support.

--- bbv/protected/components/Controller.php
+++ bbv/protected/components/Controller.php
@@ -44,6 +44,20 @@ class Controller extends CController
 		// Spin
 		$cs->registerScriptFile($baseUrl.'/js/spin.min.js');
 		
+		// Google Analytics
+		if(isset(Yii::app()->params['googleAnalyticsTrackingID'])) {
+			Yii::app()->clientScript->registerScript('googleAnalytics',"
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', '".Yii::app()->params['googleAnalyticsTrackingID']."']);
+			  _gaq.push(['_trackPageview']);
+			
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();", CClientScript::POS_END);
+		}
+		
 		return parent::beforeAction($action);
 	}
 }
--- bbv/protected/config/main.php
+++ bbv/protected/config/main.php
@@ -111,5 +111,6 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
+		//'googleAnalyticsTrackingID'=>'[Enter tracking ID to enable]',
 	),
 );
