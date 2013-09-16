889988dc2ba90fc2c5de5522d67a3bbbe81600a5
Ruben Taelman <rubensworks@gmail.com>
2013-09-16 15:11:37 +0200

    Cropping in image thumbs is now disabled.

--- bbv/protected/controllers/ImageFileItemController.php
+++ bbv/protected/controllers/ImageFileItemController.php
@@ -64,7 +64,7 @@ class ImageFileItemController extends FileItemController
 		echo CHtml::image(Yii::app()->getRequest()->getHostInfo('').Yii::app()->easyImage->thumbSrcOf($data->getFile(), array(
 				'type' => 'png',
 				'resize' => array('width' => 200, 'height' => 200),
-				'crop' => array('width' => 100, 'height' => 100),
+				//'crop' => array('width' => 100, 'height' => 100),
 			)), $data->item->name);
 		echo '</a>';
 	}
--- bbv/protected/views/imageFileItem/_list_item.php
+++ bbv/protected/views/imageFileItem/_list_item.php
@@ -2,6 +2,6 @@
 	<?php echo CHtml::image(Yii::app()->easyImage->thumbSrcOf($data->getFile(), array(
 			'type' => 'png',
 			'resize' => array('width' => 200, 'height' => 200),
-			'crop' => array('width' => 100, 'height' => 100),
+			//'crop' => array('width' => 100, 'height' => 100),
 	)), $data->item->name); ?>
 </a>
