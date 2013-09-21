59e38677368abbb5a087055cdf00959ed53acb2a
Ruben Taelman <rubensworks@gmail.com>
2013-09-21 18:29:05 +0200

    Improved styling of the FileItem _item view.

--- bbv/protected/views/fileItem/_item.php
+++ bbv/protected/views/fileItem/_item.php
@@ -1,9 +1,11 @@
 <div class="file-item-row">
-	<div class="name">
-		<i class="extension <?php echo $data->getMimeTypeClass() ?>"></i>
-		<?php echo CHtml::link($data->item->name.'.'.$data->extension, $data->getDownloadLink()); ?><br />
-	</div>
-	<div class="content">
-		<?php echo $data->item->content; ?>
+	<div class="sub-row">
+		<div class="name">
+			<i class="extension <?php echo $data->getMimeTypeClass() ?>"></i>
+			<?php echo CHtml::link($data->item->name.'.'.$data->extension, $data->getDownloadLink()); ?><br />
+		</div>
+		<div class="content">
+			<?php echo $data->item->content; ?>
+		</div>
 	</div>
 </div>
